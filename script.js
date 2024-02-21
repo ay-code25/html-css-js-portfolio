f4196df40233283af864edd36f98cf5ba87d4230
ay-code25 <ay.code25@outlook.com>
2024-02-21 11:07:40 +0000

    file edited

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
-    icon.classList.toggle("open")
+    icon.classList.toggle("open");
 }
